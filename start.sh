8e6051bcad76ee7f80b570ad449b3eaa073523cc
Nils Jarle Haugen <nils+git@gaupne.net>
2021-01-19 21:33:18 +0100

    Added a script to copy the defaults if not setup

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-#! /bin/bash
-./maven_build.sh
-docker-compose  --env-file ./config_files/main.env up --build -V
+#! /bin/sh
+# copy defaults if no env file is in place
+if [ ! -f .env ]; then
+  echo "You had no .env file, so I copied the example file."
+  cp .env-example .env
+  echo "\nThe contents of the .env:"
+  cat .env
+  echo "\nPlease wait...\n"
+  sleep 10
+fi
+
+./build-server-on.docker.sh
