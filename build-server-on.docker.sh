e5ffc2e476f91de71752fe926e2e7b2411f7aa3b
Nils Jarle Haugen <nils+git@gaupne.net>
2021-01-20 10:11:39 +0100

    cleanups

--- build-server-on.docker.sh
+++ build-server-on.docker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1 
-export COMPOSE_DEV_FILE="docker-compose-dev.yml"
+export COMPOSE_FILE="docker-compose.yml"
 
-echo "Stopping docker compose $COMPOSE_DEV_FILE"
-docker-compose -f $COMPOSE_DEV_FILE down --remove-orphans &&
-docker-compose -f $COMPOSE_DEV_FILE up -d --build database api reverse-proxy
+echo "Stopping docker compose $COMPOSE_FILE"
+docker-compose -f $COMPOSE_FILE down --remove-orphans &&
+docker-compose -f $COMPOSE_FILE up -d --build database api reverse-proxy
 echo "The was the api was rebuilt if any files changed, use CTRL-C to detach from the log console"
-docker-compose -f $COMPOSE_DEV_FILE logs -f
+docker-compose -f $COMPOSE_FILE logs -f
