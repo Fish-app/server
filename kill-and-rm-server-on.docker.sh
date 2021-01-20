091a35bfdba464a4a5b4c707ecd3e52e7ebf5f5a
Nils Jarle Haugen <nils+git@gaupne.net>
2021-01-20 10:14:07 +0100

    Small adjustments
    updated envs
    now using database setup scripts
    cleanups

--- kill-and-rm-server-on.docker.sh
+++ kill-and-rm-server-on.docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1 
-export COMPOSE_DEV_FILE="docker-compose-dev.yml"
+export COMPOSE_FILE="docker-compose.yml"
 
-echo "Stopping docker compose $COMPOSE_DEV_FILE"
-docker-compose -f $COMPOSE_DEV_FILE down --remove-orphans
+echo "Stopping docker compose $COMPOSE_FILE"
+docker-compose -f $COMPOSE_FILE down --remove-orphans
