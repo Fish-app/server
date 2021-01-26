e1ac0b6974f06785c252cbd9efcb4b16d13f61ad
trygve <trygve.woldseth@gmail.com>
2021-01-26 10:29:46 +0100

    added comodity and endpoints

---
+++ src/main/java/no/maoyi/app/commodity/entity/Commodity.java
@@ -0,0 +1,39 @@
+package no.maoyi.app.commodity.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import no.maoyi.app.listing.entity.OfferListing;
+import no.maoyi.app.listing.entity.BuyRequest;
+import no.maoyi.app.resources.entity.Image;
+
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Commodity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    BigInteger id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    String name;
+
+    @OneToOne
+    @Column(name = "commodity_image")
+    Image commodityImage;
+
+
+    @OneToMany
+    @JsonbTransient
+    List<OfferListing> offerListings;
+
+    @OneToMany
+    @JsonbTransient
+    List<BuyRequest> orderListings;
+
+
+}
