bcc7c3172440c7c6664c238750cdb9d4ea513817
trygve <trygve.woldseth@gmail.com>
2021-01-26 10:32:31 +0100

    WIP buy/sell listings

---
+++ src/main/java/no/maoyi/app/listing/entity/Listing.java
@@ -0,0 +1,51 @@
+package no.maoyi.app.listing.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import no.maoyi.app.commodity.entity.Commodity;
+import no.maoyi.app.user.entity.User;
+
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+public abstract class Listing {
+
+    protected String listingType;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    BigInteger id;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    Date created;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    User creator;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(name = "end_date")
+    Date endDate;
+
+    @ManyToOne
+    @JsonbTransient
+    Commodity commodity;
+
+    @Column(nullable = false)
+    int price;
+
+    @Column(name = "is_open")
+    Boolean isOpen;
+
+
+    @PrePersist
+    protected void onCreate() {
+        created = new Date();
+    }
+
+
+}
