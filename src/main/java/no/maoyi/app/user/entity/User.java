656c91c5ce75bb86ada8848b7e26dc70eb015512
trygve <trygve.woldseth@gmail.com>
2021-02-17 13:34:36 +0100

    seperated teh entitys

--- src/main/java/no/maoyi/app/user/entity/User.java
+++ src/main/java/no/maoyi/app/user/entity/User.java
@@ -1,7 +1,9 @@
 package no.***REMOVED***.app.user.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import no.***REMOVED***.app.auth.entity.AuthenticatedUser;
 import no.***REMOVED***.app.conversation.entity.Conversation;
 import no.***REMOVED***.app.listing.entity.Listing;
 
@@ -12,7 +14,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,47 +21,26 @@ import java.util.List;
 Represents a User in the system.
 A user has a An ID, email, first name, last name and password.
  */
-@Entity
 @Data
+@Entity
 @NoArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "users")
-@NamedQuery(name = User.USER_BY_EMAIL, query = "SELECT e FROM User e WHERE e.email = :email")
-public class User implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends AuthenticatedUser implements Serializable {
 
-    public static final String USER_BY_EMAIL = "User.getByEmail";
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private BigInteger id;
-
-    @Column
-    @Temporal(javax.persistence.TemporalType.DATE)
-    Date created;
 
     // -- User info -- //
     @NotBlank
     @Column(nullable = false)
     private String name;
 
-    @Email
-    @Column(nullable = false, unique = true)
+    //    @Email
+    //    @Column(nullable = false, unique = true)
+    //    private String email;
+
+    @Transient
     private String email;
-
-    @Size(min = 6)
-    @JsonbTransient
-    @Column(nullable = false)
-    private String password;
-
-    @ManyToMany
-    @JsonbTransient
-    @JoinTable(
-            name = "user_groups",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "groups_name", referencedColumnName = "name"))
-    List<Group> groups;
-
 
     // -- User data -- //
 
@@ -73,25 +53,25 @@ public class User implements Serializable {
     @JsonbTransient
     List<Listing> userCreatedOrders;
 
+    ////////////////////////////////////////
+    // todo: unshure if horrible design
 
-    public User(String email, String name, String password) {
-        System.out.println("----------------------------------------------------------------------------------------");
+    public void setEmail(String email) {
+        this.email = email;
+        this.setPrincipalName(email);
+    }
+
+    @PostLoad
+    private void setMailToPrincipal() {
+        this.email = this.getPrincipalName();
+    }
+
+    ///////////////////////////////////////
+
+
+    protected User(String email, String name, String password) {
         this.setEmail(email);
         this.setName(name);
         this.setPassword(password);
     }
-    
-    public List<Group> getGroups() {
-        if (groups == null) {
-            groups = new ArrayList<>();
-        }
-        return groups;
-    }
-
-
-    @PrePersist
-    protected void onCreate() {
-        created = new Date();
-    }
-
 }
