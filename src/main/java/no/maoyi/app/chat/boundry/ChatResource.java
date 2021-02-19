d61a060fe8be0db65de7511bff10c3e4e7c439ee
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-19 15:58:40 +0100

    implementing backend logics

--- src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
+++ src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
@@ -1,4 +1,42 @@
 package no.maoyi.app.chat.boundry;
 
+import no.maoyi.app.auth.entity.Group;
+import no.maoyi.app.chat.control.ChatService;
+import no.maoyi.app.chat.entity.Conversation;
+import no.maoyi.app.listing.control.ListingService;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("chat")
 public class ChatResource {
+
+    @Inject
+    ChatService service;
+
+
+    @POST
+    @RolesAllowed(value = {Group.USER_GROUP_NAME,Group.ADMIN_GROUP_NAME})
+    @Path("start")
+    public Response startConversation(
+            @NotNull @FormDataParam("listing") long listingId
+    ) {
+        Response response = Response.serverError().build();
+        Conversation conversation = null;
+        conversation = service.newConversation();
+        if (conversation != null) {
+            conversation = service.attachConversation(conversation,listingId);
+            if (conversation != null) {
+                response = Response.ok(conversation).build();
+            }
+        }
+        return response;
+    }
 }
