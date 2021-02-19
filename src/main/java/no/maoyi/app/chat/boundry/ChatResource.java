f273fe76073820e1ccaab96390da9e0cbed89c9c
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-19 15:58:40 +0100

    implementing backend logics

--- src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
+++ src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
@@ -1,4 +1,42 @@
 package no.***REMOVED***.app.chat.boundry;
 
+import no.***REMOVED***.app.auth.entity.Group;
+import no.***REMOVED***.app.chat.control.ChatService;
+import no.***REMOVED***.app.chat.entity.Conversation;
+import no.***REMOVED***.app.listing.control.ListingService;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("chat")
 public class ChatResource {
+
+    @Inject
+    ChatService service;
+
+
+    @POST
+    @RolesAllowed(value = {Group.USER_GROUP_NAME,Group.ADMIN_GROUP_NAME})
+    @Path("start")
+    public Response startConversation(
+            @NotNull @FormDataParam("listing") long listingId
+    ) {
+        Response response = Response.serverError().build();
+        Conversation conversation = null;
+        conversation = service.newConversation();
+        if (conversation != null) {
+            conversation = service.attachConversation(conversation,listingId);
+            if (conversation != null) {
+                response = Response.ok(conversation).build();
+            }
+        }
+        return response;
+    }
 }
--- src/main/java/no/maoyi/app/chat/control/ChatService.java
+++ src/main/java/no/maoyi/app/chat/control/ChatService.java
@@ -2,34 +2,90 @@ package no.***REMOVED***.app.chat.control;
 
 import no.***REMOVED***.app.chat.entity.Conversation;
 import no.***REMOVED***.app.chat.entity.Message;
+import no.***REMOVED***.app.listing.entity.Listing;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatService {
 
     @PersistenceContext
     EntityManager em;
 
-    Conversation findConversationById(Long id) {
-        // Select all converstations in db
-        // Return first result if found, otherwise return null
-        return null;
+
+    public Conversation newConversation() {
+        Conversation input = new Conversation();
+        if (addToDB(input)) {
+            return input;
+        } else {
+            return null;
+        }
     }
 
-    boolean addMessage(Message message, Conversation conversation) {
+    public Conversation attachConversation(Conversation conversation, long listingId) {
+        Listing baseOrder =  em.find(Listing.class, listingId);
+        if ( baseOrder == null | conversation == null) return null;
+        conversation.setBaseOrder(baseOrder);
+        return updateToDB(conversation);
+
+    }
+
+    Message addMessage(Message message, Conversation conversation) {
         // Find the conversation by id
         // If found, add a message to the List inside the conversation
-        return false;
+        Conversation foundConversation = getFromDB(conversation);
+        if (foundConversation == null || message == null) return null;
+        List<Message> msgList = getMsgs(foundConversation);
+        msgList.add(message);
+        em.persist(message);
+        foundConversation = updateToDB(foundConversation);
+        if(foundConversation == null) {
+            return null;
+        } else {
+            return message;
+        }
     }
 
-    void attachImageToMessage(Message message) {
-        // Fin the message by id, and attach a image to the m
+
+    private boolean addToDB(Conversation input) {
+        try {
+            em.persist(input);
+            return true;
+        } catch (PersistenceException pe) {
+            return false;
+        }
+    }
+
+    private Conversation updateToDB(Conversation input) {
+        try {
+            Conversation output = em.merge(input);
+            return output;
+        } catch (PersistenceException pe) {
+            return null;
+        }
+    }
+
+    private Conversation getFromDB(Conversation input) {
+        try {
+           return em.find(Conversation.class, input);
+        } catch (PersistenceException pe) {
+            return null;
+        }
     }
 
 
-    Message getSingleMessageById(Long id) {
-        // Find the message by id with JPA and return message if found
-        return null;
+    private List<Message> getMsgs(Conversation conversation) {
+        Conversation result = em.find(Conversation.class, conversation);
+        if (result == null) return null;
+        List<Message> messageList = result.getMessages();
+        if (messageList == null) {
+            messageList = new ArrayList<>();
+        }
+        return messageList;
     }
+
+
 }
