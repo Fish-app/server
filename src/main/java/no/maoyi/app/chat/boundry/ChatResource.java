7aa14b5fa3deebe55bf53ef4c9f3d72438d874e5
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-21 23:19:55 +0100

    Implementing logic, testing not passing

--- src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
+++ src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
@@ -3,6 +3,8 @@ package no.***REMOVED***.app.chat.boundry;
 import no.***REMOVED***.app.auth.entity.Group;
 import no.***REMOVED***.app.chat.control.ChatService;
 import no.***REMOVED***.app.chat.entity.Conversation;
+import no.***REMOVED***.app.user.control.UserService;
+import no.***REMOVED***.app.user.entity.User;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
@@ -13,7 +15,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.security.Principal;
 
 @Stateless
 @Path("chat")
@@ -21,6 +22,9 @@ public class ChatResource {
 
     @Inject
     ChatService service;
+
+    @Inject
+    UserService userService;
 
 
     @POST
@@ -51,23 +55,27 @@ public class ChatResource {
             @FormParam("body") String messageBody
             // TODO: Change body to a more fitting type
     ) {
-        Response response = Response.serverError().build();
+        Response response = Response.status(Response.Status.BAD_REQUEST).build();
 
         if ((conversationId == null) ^ (listingId == null)) {
             // do request
             // if conversationid is null, listing must not be null and vice versa
+            User senderUser = userService.getLoggedInUser();
             if ((conversationId != null)) {
                 // sender knows conversation id, and wants to send message directly
+                System.out.println("RESOURCE-CHAT: msg -> conversation");
                 service.sendMessageToConversation(senderUser, messageBody, conversationId);
             } else {
                 // sender knows listing id, and wants to send message to conversation,
                 // by being identified with userid and listing (1 user can start 1 conversation on 1 listing) (overkill?)
+                System.out.println("RESOURCE-CHAT: msg -> listing");
                 service.sendMessageToListing(senderUser, messageBody, listingId);
             }
         } else {
             // bad request (either both or none are specified)
             return response;
        }
+        return  response;
     }
 
 }
