f6574e88b3f9e81c8b088781b58b52722a4bd9be
trygve <trygve.woldseth@gmail.com>
2021-02-23 14:32:56 +0100

    suggest

--- src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
+++ src/main/java/no/maoyi/app/chat/boundry/ChatResource.java
@@ -4,24 +4,28 @@ import no.maoyi.app.auth.entity.Group;
 import no.maoyi.app.chat.control.ChatService;
 import no.maoyi.app.chat.entity.Conversation;
 import no.maoyi.app.chat.entity.ConversationDTO;
+import no.maoyi.app.chat.entity.Message;
 import no.maoyi.app.chat.entity.MessageDTO;
 import no.maoyi.app.user.control.UserService;
-import no.maoyi.app.user.entity.User;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Stateless
 @Path("chat")
+@RolesAllowed(value = {Group.USER_GROUP_NAME, Group.ADMIN_GROUP_NAME})
+
 public class ChatResource {
 
     @Inject
-    ChatService service;
+    ChatService chatService;
 
     @Inject
     UserService userService;
@@ -32,94 +36,83 @@ public class ChatResource {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Path("startconversation")
     public Response startConversationRequest(
-            @FormParam("listing") Long listingId
+            @NotNull @HeaderParam("listing") long listingId
     ) {
-        Response response = Response.serverError().build();
+        Response     response;
         Conversation conversation = null;
-        conversation = service.newConversation();
+        conversation = chatService.newListingConversation(listingId);
         if (conversation != null) {
-            if (listingId != null) conversation = service.addConversationToListing(conversation, listingId);
-            if (conversation != null) {
-                response = Response.ok(new ConversationDTO(conversation)).build();
-            }
+            response = Response.ok(new ConversationDTO(conversation)).build();
+        } else {
+            response = Response.serverError().build();
         }
         return response;
     }
 
-    /**
-     * Send a message to a conversation by ID or a conversation associated with a listing ID
-     *
-     * @param conversationId the conversation to send the message to
-     * @param messageBody    currently message text
-     * @return 200 OK if successful, with the conversation DTO. App can use math message count
-     * to find out how many messages it needs to ask for in @GET getmessages()
-     */
     @POST
-    @RolesAllowed(value = {Group.USER_GROUP_NAME, Group.ADMIN_GROUP_NAME})
-    @Path("sendmessage")
+    @Path("new")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response sendMessageRequest(
-            @FormParam("conversation") Long conversationId,
-            @FormParam("listing") Long listingId,
-            @FormParam("body") String messageBody
-            // TODO: Change body to a more fitting type
+            @NotNull @HeaderParam("conversation") long conversationId,
+            @NotNull @HeaderParam("body") String messageBody
     ) {
-        // 500 will be called on failure (Pajara or Jakarta will return 500 on exception)
-        Response response = Response.status(Response.Status.BAD_REQUEST).build();
-        User senderUser = userService.getLoggedInUser();
+        Response     response;
+        Conversation conversation = chatService.getConversation(conversationId);
 
-        // return 401 if not logged in
-        if (senderUser == null) response = Response.status(Response.Status.UNAUTHORIZED).build();
+        if (conversation.isUserInConversation(userService.getLoggedInUser())) {
+            chatService.sendMessage(messageBody, conversation);
+            response = Response.ok().build();
+        } else {
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
+        }
 
-        if ((conversationId != null)) {
-            // sender knows conversation id, and wants to send message directly
-            System.out.println("RESOURCE-CHAT: msg -> conversation");
-            ConversationDTO result = service.sendMessageToConversation(senderUser, messageBody, conversationId);
-            if (result != null) {
-                System.out.println("RESOURCE-CHAT: 200 OK");
-                response = Response.ok(result).build();
+        return response;
+    }
+
+    @GET
+    @Path("mabyUpdates")
+    public Response updatesQuery(
+            @NotNull @HeaderParam("conversation") long conversationId,
+            @NotNull @HeaderParam("last-id") long lastId
+    ) {
+        Response     response;
+        Conversation conversation = chatService.getConversation(conversationId);
+
+        if (conversation.isUserInConversation(userService.getLoggedInUser())) {
+            if (conversation.getLastMessageId() == lastId) {
+                response = Response.ok().build();
             } else {
-                response = Response.notModified().build();
+                List<Message> messages = chatService.getMessagesTo(conversationId, lastId);
+                List<MessageDTO> messageDTOS = messages.stream()
+                                                       .map(MessageDTO::buildFromMessage)
+                                                       .collect(Collectors.toList());
+                response = Response.ok(messageDTOS).build();
             }
         } else {
-            // bad request (either both or none are specified)
-            return response;
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
         }
+
         return response;
     }
 
 
-    /**
-     * Returns a list of the messages the client asked for
-     *
-     * @param conversationId the id of the conversation to find messages in
-     * @param rangeEnd       .
-     * @param rangeStart     .
-     * @return a conversation DTO holding list of messages, or a plain JSON list of List(Message)  ?
-     * probably better with a plain list, as the conversation DTO was returned before
-     */
-    @GET
-    @RolesAllowed(value = {Group.USER_GROUP_NAME, Group.ADMIN_GROUP_NAME})
-    @Path("getmessages")
-    public Response getFilteredMessagesInConversation(
-            @QueryParam("conversation") Long conversationId,
-            @QueryParam("to") Long rangeEnd,
-            @QueryParam("from") Long rangeStart
+    public Response getMessageRange(
+            @NotNull @HeaderParam("conversation") long conversationId,
+            @NotNull @HeaderParam("from") long fromId,
+            @NotNull @HeaderParam("offset") long offset
     ) {
-        User receiver = userService.getLoggedInUser();
-        Response response = Response.ok().build();
+        Response     response;
+        Conversation conversation = chatService.getConversation(conversationId);
 
-        // 401 if not logged in
-        if (receiver == null) return Response.status(Response.Status.UNAUTHORIZED).build();
-
-        // 1. check if receiver is member of conversation
-        // 2. check if range parameters is specified, and filter the message list
-        //    otherwise return all messages in conversation
-        // 3. Return the list of messages in the response
-
-        // e.g.
-        //List<MessageDTO> msgList = service.getMessagesInRange(receiver, conversationId, rangeStart, rangeEnd);
-        //response = Response.ok(msgList).build();
+        if (conversation.isUserInConversation(userService.getLoggedInUser())) {
+            List<Message> messages = chatService.getMessageRange(conversationId, fromId, offset);
+            List<MessageDTO> messageDTOS = messages.stream()
+                                                   .map(MessageDTO::buildFromMessage)
+                                                   .collect(Collectors.toList());
+            response = Response.ok(messageDTOS).build();
+        } else {
+            response = Response.status(Response.Status.UNAUTHORIZED).build();
+        }
 
         return response;
     }
--- src/main/java/no/maoyi/app/chat/entity/ConversationDTO.java
+++ src/main/java/no/maoyi/app/chat/entity/ConversationDTO.java
@@ -4,7 +4,6 @@ package no.maoyi.app.chat.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import no.maoyi.app.listing.entity.Listing;
 
 import javax.json.bind.annotation.JsonbTransient;
 import java.util.List;
@@ -14,10 +13,10 @@ import java.util.stream.Collectors;
 /**
  * This and the message DTO exists so the message backlog does not have to include the
  * user object of the sender once per message, when all that's needed is the user id
- *
- *  Returns metadata about a conversation; what order asscoiated, the current count of msgs
- *  so that the client can send an GET message list seperatly with only the messages it want to know
- *  FIXME: messages should be sent in a seperate DTO/ JSON list ??
+ * <p>
+ * Returns metadata about a conversation; what order asscoiated, the current count of msgs
+ * so that the client can send an GET message list seperatly with only the messages it want to know
+ * FIXME: messages should be sent in a seperate DTO/ JSON list ??
  */
 @Data
 @NoArgsConstructor
@@ -25,21 +24,18 @@ import java.util.stream.Collectors;
 public class ConversationDTO {
 
     public ConversationDTO(Conversation conversation) {
-        this.id        = conversation.getId();
-        this.baseOrder = conversation.getBaseOrder();
-        this.version = conversation.getVersion();
+        this.id = conversation.getId();
         this.messageCount = conversation.messages.size();
-        this.messages  = conversation.messages.stream()
-                                              .parallel()
-                                              .map(MessageDTO::buildFromMessage)
-                                              .collect(Collectors.toList());
+        this.messages = conversation.messages.stream()
+                                             .parallel()
+                                             .map(MessageDTO::buildFromMessage)
+                                             .collect(Collectors.toList());
     }
 
     long id;
 
     long version;
 
-    Listing baseOrder;
 
     // FIXME: better to use message list in a seperate endpoint ?
     @JsonbTransient
