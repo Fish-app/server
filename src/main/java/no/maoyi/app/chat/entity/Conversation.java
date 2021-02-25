7ef0472582f40b3edba65abc99459ff70fbc8d9f
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-25 11:57:55 +0100

    save unsaved changes (WIP, not finished)

--- src/main/java/no/maoyi/app/chat/entity/Conversation.java
+++ src/main/java/no/maoyi/app/chat/entity/Conversation.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import no.***REMOVED***.app.listing.entity.Listing;
 import no.***REMOVED***.app.user.entity.User;
 
+import javax.json.bind.annotation.JsonbAnnotation;
+import javax.json.bind.annotation.JsonbProperty;
 import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
 import java.util.ArrayList;
