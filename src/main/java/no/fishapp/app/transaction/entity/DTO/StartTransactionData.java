1e3ad165fdb091628debb3e0556f4f56f9382314
trygve <trygve.woldseth@gmail.com>
2021-03-09 09:35:26 +0100

    fix

--- src/main/java/no/fishapp/app/transaction/entity/DTO/StartTransactionData.java
+++ src/main/java/no/fishapp/app/transaction/entity/DTO/StartTransactionData.java
@@ -1,12 +1,16 @@
 package no.fishapp.app.transaction.entity.DTO;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StartTransactionData {
 
     @NonNull
