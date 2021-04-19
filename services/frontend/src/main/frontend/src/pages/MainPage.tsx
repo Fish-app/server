ffd9d8999aa61161891dda7b40d3bc802e70b2e3
trygve <trygve.woldseth@gmail.com>
2021-04-19 14:58:12 +0200

    added logo

--- services/frontend/src/main/frontend/src/pages/MainPage.tsx
+++ services/frontend/src/main/frontend/src/pages/MainPage.tsx
@@ -64,19 +64,17 @@ export function MainPage() {
         <Header>
             <div className="logo">Fishapp</div>
             <Menu theme="dark" mode="horizontal" selectedKeys={[currentIndex.toString()]}>
-                <Menu.Item key="1" onClick={info => history.push(`${url}/home`)}>nav 1</Menu.Item>
-                <Menu.Item key="2" onClick={info => history.push(`${url}/user`)}>nav 2</Menu.Item>
-                <Menu.Item key="3" onClick={info => history.push(`${url}/commodity`)}>nav 3</Menu.Item>
+                <Menu.Item key="1" onClick={info => history.push(`${url}/home`)}>Home</Menu.Item>
+                <Menu.Item key="2" onClick={info => history.push(`${url}/user`)}>User</Menu.Item>
+                <Menu.Item key="3" onClick={info => history.push(`${url}/commodity`)}>Commodity</Menu.Item>
             </Menu>
         </Header>
         <Content style={{padding: '0 50px'}}>
             <Breadcrumb style={{margin: '16px 0'}}>
-
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>List</Breadcrumb.Item>
                 <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-
             <div className="site-layout-content">
 
                 <Switch>
@@ -85,50 +83,6 @@ export function MainPage() {
                     </Route>
                 </Switch>
             </div>
-
         </Content>
-
     </Layout>
-
-
-    // <Layout>
-    //     <Sider
-    //         breakpoint="lg"
-    //         collapsedWidth="0"
-    //         onBreakpoint={broken => {
-    //             console.log(broken);
-    //         }}
-    //         onCollapse={(collapsed, type) => {
-    //             console.log(collapsed, type);
-    //         }}
-    //     >
-    //         <div className="logo">
-    //             <h2>Fishapp</h2>
-    //         </div>
-    //         <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-    //             <Menu.Item key="1" icon={<UserOutlined/>}>
-    //                 nav 1
-    //             </Menu.Item>
-    //             <Menu.Item key="2" icon={<VideoCameraOutlined/>}>
-    //                 nav 2
-    //             </Menu.Item>
-    //             <Menu.Item key="3" icon={<UploadOutlined/>}>
-    //                 nav 3
-    //             </Menu.Item>
-    //             <Menu.Item key="4" icon={<UserOutlined/>}>
-    //                 nav 4
-    //             </Menu.Item>
-    //         </Menu>
-    //     </Sider>
-    //     <Layout>
-    //         <Header className="site-layout-sub-header-background" style={{padding: 0}}/>
-    //         <Content style={{margin: '24px 16px 0'}}>
-    //             <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-    //                 content
-    //             </div>
-    //         </Content>
-    //         <Footer style={{textAlign: 'center'}}>XXXX FILL ME IN LATER XXXX</Footer>
-    //     </Layout>
-    // </Layout>
-
 }
